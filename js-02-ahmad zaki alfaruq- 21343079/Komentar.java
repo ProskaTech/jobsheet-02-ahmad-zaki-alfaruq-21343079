183526375cf57bacbb335cd6c4926995a6eecbef
ahmad zaki alfaruq- 21343079 <112170461+ahmadzaki29@users.noreply.github.com>
2022-09-15 22:31:08 +0700

    Add files via upload

---
+++ js-02-ahmad zaki alfaruq- 21343079/Komentar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ * Created by Ahmad Zaki Alfaruq 21343079
+ */
+public class Komentar{
+    public static void main(String[] args) {
+        System.out.println("Singel line comment above");
+        // Comment line 1
+        System.out.println("Multi line comments below");
+        /*Comment line 2
+          Comment line 3
+          Comment line 4*/
+    }
+}
---
+++ js-02-ahmad zaki alfaruq- 21343079/Tugass2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ * Created by Ahmad Zaki Alfaruq
+ */
+public class Tugass2 {
+    public static void main(String args[]){
+        String nama = "Farraz Hadyan";
+        String matkul = "Praktikum PBO";
+        String nim = "21343045";
+        
+        System.out.println("DATA MAHASISWA"); //Judul 
+        System.out.println(nama);
+        System.out.println(matkul);
+        System.out.println(nim);
+        /*nama, 
+        matkul, 
+        dan nim mahasiswa*/
+    }
+    
+}
